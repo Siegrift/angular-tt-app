4a332dae6d680e10d52bba77c4e1912752248b98
Emanuel Tesar <siegrift@google.com>
2019-08-08 19:24:11 +0200

    Add semicolons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,13 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('linkText') linkText: ElementRef;
-  unsafeLink: string
+  @ViewChild('linkText', {static: false}) linkText: ElementRef;
+  unsafeLink: string;
+
+  interpolatingNullValue = null;
 
   // try: http://localhost:4200/xss?html=<img src=x onerror=alert(0) />
   changeLink(link) {
-    this.unsafeLink = 'html?' + link
+    this.unsafeLink = 'html?' + link;
   }
 }
--- src/app/trustedTypesPolicy.ts
+++ src/app/trustedTypesPolicy.ts
@@ -1,27 +1,30 @@
-let componentPolicy: any
+let componentPolicy: any;
 
 export function getPolicy(): any {
   if (componentPolicy === undefined) {
     const rules = {
       createHTML(s: string): string {
-        console.log('App createHTML', s)
+        console.log('App createHTML', s);
         return s;
       },
       createURL(s: string): string {
-        console.log('App createURL', s)
+        console.log('App createURL', s);
         return s;
       },
       createScriptURL(s: string): string {
-        console.log('App createScriptURL', s)
+        console.log('App createScriptURL', s);
         return s;
       },
       createScript(s: string): string {
-        console.log('App createScript', s)
+        console.log('App createScript', s);
         return s;
       },
+    };
+    if (typeof TrustedTypes === 'undefined') {
+      componentPolicy = rules;
+    } else {
+      componentPolicy = TrustedTypes.createPolicy('app-root-policy', rules);
     }
-    if (typeof TrustedTypes === 'undefined') componentPolicy = rules;
-    else componentPolicy = TrustedTypes.createPolicy('app-root-policy', rules);
   }
-  return componentPolicy
+  return componentPolicy;
 }
